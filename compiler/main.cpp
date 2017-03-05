69468088602180c00543fd76ccba8c9ff336dfb9
jac259 <jac259@zips.uakron.edu>
2017-03-04 19:21:35 -0500

    Removed unused code.

--- compiler/main.cpp
+++ compiler/main.cpp
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <sstream>
 
+// See the GitHub README if instructions to use this program are required.
+// https://github.com/jac259/CompilerDesign/blob/master/README.md
+
 int main(int argc, char * argv[]) {
 
   char outputType = 'd';
@@ -39,32 +42,5 @@ int main(int argc, char * argv[]) {
     output.str(std::string());
   }
 
-  /*  
-    Expr * T = new Bool_Expr(true);
-  Expr * F = new Bool_Expr(false);
-  Expr * _10 = new Int_Expr(10);
-  Expr * _25 = new Int_Expr(25);
-
-  Expr * bit_and = new Bit_And_Expr(T, F);
-  Expr * bit_or = new Bit_Or_Expr(T, F);
-  Expr * bit_xor = new Bit_Xor_Expr(T, F);
-  Expr * bit_comp = new Bit_Comp_Expr(T);
-
-  Expr * bit_and_int = new Bit_And_Expr(_10, _25);
-  Expr * bit_or_int = new Bit_Or_Expr(_10, _25);
-  Expr * bit_xor_int = new Bit_Xor_Expr(_10, _25);
-  Expr * bit_comp_int = new Bit_Comp_Expr(_10);
-
-  std::cout << "Bitwise AND (T & F): " << std::boolalpha << (bool)bit_and->Eval() << "(" << bit_and->Eval() << ")\n";
-  std::cout << "Bitwise AND (10 & 25): " << bit_and_int->Eval() << "\n";
-  std::cout << "Bitwise OR (T | F): " << std::boolalpha << (bool)bit_or->Eval() << "(" << bit_or->Eval() << ")\n";
-  std::cout << "Bitwise OR (10 | 25): " << bit_or_int->Eval() << "\n";
-  std::cout << "Bitwise XOR (T ^ F): " << std::boolalpha << (bool)bit_xor->Eval() << "(" << bit_xor->Eval() << ")\n";
-  std::cout << "Bitwise XOR (10 ^ 25): " << bit_xor_int->Eval() << "\n";
-  std::cout << "Bitwise COMP (~T): " << std::boolalpha << (bool)bit_comp->Eval() << "(" << bit_comp->Eval() << ")\n";
-  std::cout << "Bitwise COMP (~10): " << bit_comp_int->Eval() << "\n";
-
-  std::cout << "-------------------------------------\n";
-  */
   return 1;
 }
